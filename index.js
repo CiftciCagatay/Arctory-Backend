4a0acda2d77984ead1235e329c335399dba6ee5f
Cagatay Ciftci <im.cgtycftc@gmail.com>
2018-02-17 02:57:08 +0300

    Add lessonController route to application

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 
+const lessonController = require('./src/lesson/index')
+
 const app = express()
 
 app.use(bodyParser.json())
 app.set('port', process.env.PORT || 5000)
+
+app.use('/lessons', lessonController)
 
 app.listen(app.get('port'), () => console.log(`App is listening port ${app.get('port')}...`))

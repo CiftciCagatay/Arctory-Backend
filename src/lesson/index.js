fd2763846c6efa25aacf33dedbdb80bae4da09fa
Cagatay Ciftci <im.cgtycftc@gmail.com>
2018-02-17 03:56:39 +0300

    Update lessonController add getLessons service

--- src/lesson/index.js
+++ src/lesson/index.js
@@ -1,4 +1,22 @@
 const express = require('express')
 const router = express.Router()
 
+const getLessons = require('./dbQueries/getLessons')
+
+router.get('/', (req, res, next) => {
+  const maxFee = JSON.parse(req.query.maxFee)
+  
+  getLessons(req.query.searchString, maxFee, req.query.meetingPoint)
+    .then((result) => {
+      console.log(result)
+      res.status(200).json({ result })
+      next()
+    })
+    .catch((error) => {
+      console.log(error)
+      res.status(500).json({ error })
+      next()
+    })
+})
+
 module.exports = router

a805c4892bb00e3f9919d6b5c387ab0e3ceb459e
Cagatay Ciftci <im.cgtycftc@gmail.com>
2018-02-17 03:28:49 +0300

    Add database connect and disconnect methods to app by routes in index file

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const db = require('./db')
 
 const lessonController = require('./src/lesson/index')
 
@@ -8,6 +9,17 @@ const app = express()
 app.use(bodyParser.json())
 app.set('port', process.env.PORT || 5000)
 
+// Database connection
+app.use('/', (req, res, next) => {
+  db.connectToMongo()
+    .then(next())
+    .catch((error) => res.status(500).json({ error }))
+})
+
+// Routers
 app.use('/lessons', lessonController)
+
+// Database disconnection
+app.use('/', db.disconnectFromMongo)
 
 app.listen(app.get('port'), () => console.log(`App is listening port ${app.get('port')}...`))

34aee0a76974a37d97052026f7c2b285d00374db
Cagatay Ciftci <im.cgtycftc@gmail.com>
2018-02-17 04:06:32 +0300

    Update getLessons service Add error handling for maxFee parsing

--- src/lesson/index.js
+++ src/lesson/index.js
@@ -4,19 +4,27 @@ const router = express.Router()
 const getLessons = require('./dbQueries/getLessons')
 
 router.get('/', (req, res, next) => {
-  const maxFee = JSON.parse(req.query.maxFee)
-  
-  getLessons(req.query.searchString, maxFee, req.query.meetingPoint)
-    .then((result) => {
-      console.log(result)
-      res.status(200).json({ result })
-      next()
-    })
-    .catch((error) => {
-      console.log(error)
-      res.status(500).json({ error })
-      next()
-    })
+    const maxFee = 0
+    
+    if (req.query.maxFee) {
+      try {
+        maxFee = JSON.parse(req.query.maxFee)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    getLessons(req.query.searchString, maxFee, req.query.meetingPoint)
+      .then((result) => {
+        console.log(result)
+        res.status(200).json({ result })
+        next()
+      })
+      .catch((error) => {
+        console.log(error)
+        res.status(500).json({ error })
+        next()
+      })
 })
 
 module.exports = router
